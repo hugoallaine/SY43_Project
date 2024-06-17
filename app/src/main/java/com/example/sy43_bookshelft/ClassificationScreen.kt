a515d9ae1592c7c1e40fab4d016a62fe44a0b4e2
HugoPlayer <hugo+github@allaine.cc>
2024-06-17 23:41:10 +0200

    TopBar ClassificationScreen

--- app/src/main/java/com/example/sy43_bookshelft/ClassificationScreen.kt
+++ app/src/main/java/com/example/sy43_bookshelft/ClassificationScreen.kt
@@ -11,12 +11,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -28,6 +31,7 @@ import java.io.FileWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ClassificationScreen(navController: NavHostController) {
     val context = LocalContext.current
@@ -41,11 +45,32 @@ fun ClassificationScreen(navController: NavHostController) {
             verticalArrangement = Arrangement.Top,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
         ) {
-            Text("Edit classifications", fontSize = 20.sp, modifier = Modifier.padding(bottom = 16.dp))
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        text = "Edit classifications"
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.back_btn)
+                        )
+                    }
+                },
+            )
 
-            LazyColumn(modifier = Modifier.weight(1f)) {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp)
+            ) {
                 items(classifications.toList()) { (name, regex) ->
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
